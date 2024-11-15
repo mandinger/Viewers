b8794551fc2bd332aba14abd52b34e756714d294
Nicolas Guerrero <nicoolasguerrero@gmail.com>
2024-11-15 13:31:43 -0300

    ending login method

--- platform/api/src/controllers/microsoftServiceController.js
+++ platform/api/src/controllers/microsoftServiceController.js
@@ -1,31 +1,27 @@
-// src/controllers/microsoftServiceController.js
 const qs = require('qs');
 require('dotenv').config();
 
 // Función para obtener el token de Microsoft
 const appLogin = async (req, res) => {
-  const clientId = process.env.CLIENT_ID;
-  const clientSecret = process.env.CLIENT_SECRET;
-  const tenantId = process.env.TENANT_ID;
-  const grantType = process.env.GRANT_TYPE;
-  const scope = process.env.SCOPE;
-
-  const tokenUrl = `https://login.microsoftonline.com/${tenantId}/oauth2/token`;
+  const _clientId = process.env.CLIENT_ID;
+  const _clientSecret = process.env.CLIENT_SECRET;
+  const _tenantId = process.env.TENANT_ID;
+  const _grantType = process.env.GRANT_TYPE;
+  const _scope = process.env.SCOPE;
+  const _resource = process.env.RESOURCE;
+  let _urlLoguin = process.env.URL_LOGIN;
 
   const params = {
-    client_id: clientId,
-    client_secret: clientSecret,
-    grant_type: grantType,
-    scope: scope,
+    client_id: _clientId,
+    client_secret: _clientSecret,
+    grant_type: _grantType,
+    resource: _resource,
+    scope: _scope,
   };
 
   try {
-    console.log('clientId', clientId);
-    console.log('clientSecret', clientSecret);
-    console.log('scope', scope);
-    console.log('tokenUrl', tokenUrl);
-
-    const response = await fetch(tokenUrl, {
+    _urlLoguin = _urlLoguin.replace('tenantId', _tenantId);
+    const response = await fetch(_urlLoguin, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
@@ -41,7 +37,7 @@ const appLogin = async (req, res) => {
     const data = await response.json();
 
     // Retornar el token
-    res.json({ access_token: data.access_token });
+    res.json(data);
   } catch (error) {
     console.error('Error al obtener el token:', error);
     res.status(500).json({ error: 'No se pudo obtener el token de acceso' });
--- platform/api/src/index.js
+++ platform/api/src/index.js
@@ -2,15 +2,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const microsoftServiceRoutes = require('./routes/microsoftServiceRoutes');
-require('dotenv').config(); // Cargar variables de entorno del archivo .env
+const cors = require('cors');
+
+// Cargar variables de entorno del archivo .env
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
+
 // Middleware para parsear el cuerpo de la solicitud
 app.use(bodyParser.json());
 
-// Usar las rutas definidas en microsoftServiceRoutes
+// Definicion de rutas
 app.use('/microsoftservice', microsoftServiceRoutes);
 
 // Iniciar el servidor
