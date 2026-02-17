bd49365ff02280634d9de8c8ebca664f2d4c78ca
faris <11081947+frshatat@users.noreply.github.com>
2026-02-17 13:57:54 -0700

    undo comment url

--- extensions/cornerstone/src/utils/DicomFileUploader.ts
+++ extensions/cornerstone/src/utils/DicomFileUploader.ts
@@ -174,8 +174,8 @@ export default class DicomFileUploader extends PubSubService {
           });
           console.log('📄 [DicomFileUploader] DICOM metadata:', data);
           
-          // const _apiUrl = 'https://kumo-api.ashycliff-3915e68d.eastus.azurecontainerapps.io/';
-          const _apiUrl = 'http://localhost:5500/';
+          const _apiUrl = 'https://kumo-api.ashycliff-3915e68d.eastus.azurecontainerapps.io/';
+          // const _apiUrl = 'http://localhost:5500/';
           const _uploadEndpoint = 'dataVerseService/manageUploads';
           const _tokenEndpoint = 'microsoftservice/appLoginReadWrite';
           const _urlUpload = _apiUrl + _uploadEndpoint;
